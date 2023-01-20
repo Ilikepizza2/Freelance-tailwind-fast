4aba449cda5d4d8ad195816dce0828e158728a97
Ilikepizza2 <lit2021010@iiitl.ac.in>
2023-01-20 19:55:06 +0530

    third

--- src/Components/SettingsNav.jsx
+++ src/Components/SettingsNav.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const SettingsNav = () => {
   return (
-    <div className='bg-white flex divide-gray-300 divide-x-[1px] w-fit rounded-lg mt-2 outline outline-1 outline-gray-300 text-gray-700 font-semibold text-sm max-w-full min-h-10'>
+    <div>
+    <div className='bg-white flex divide-gray-300 divide-x-[1px] w-fit rounded-lg mt-2 outline outline-1 outline-gray-300 text-gray-700 font-semibold text-sm max-w-full overflow-x-auto'>
         <div className='p-3 min-w-fit cursor-pointer hover:bg-gray-200'>My details</div>
         <div className='p-3 min-w-fit cursor-pointer hover:bg-gray-200'>Profile</div>
         <div className='p-3 min-w-fit cursor-pointer hover:bg-gray-200'>Password</div>
@@ -12,6 +13,7 @@ const SettingsNav = () => {
         <div className='p-3 min-w-fit cursor-pointer hover:bg-gray-200'>Notifications</div>
         <div className='p-3 min-w-fit cursor-pointer hover:bg-gray-200'>Integrations</div>
         <div className='p-3 min-w-fit cursor-pointer hover:bg-gray-200'>API</div>
+    </div>
     </div>
   )
 }
